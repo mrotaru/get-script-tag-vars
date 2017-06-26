18f96d161a4b0770ee74928d273006cc9a3fa7af
Mihai Rotaru <mihai.rotaru@gmx.com>
2017-06-26 22:05:11 +0100

    use tape for tests, update package.json

--- test.js
+++ test.js
@@ -1,29 +1,27 @@
 const getScriptTagVars = require('./index.js')
-const assert = require('assert')
+const test = require('tape')
 
-assert.deepEqual(getScriptTagVars('<html><script>var foo="foo"</script></html>', 'foo'), {
-  foo: 'foo',
-})
-
-assert.deepEqual(getScriptTagVars('<html><script>var foo=null</script></html>', 'foo'), {
-  foo: null,
-})
-
-assert.deepEqual(getScriptTagVars('<html><script></script></html>', 'foo'), {
-  foo: undefined,
-})
-
-assert.deepEqual(getScriptTagVars('<html><script>var foo="foo"</script></html>', ['foo', 'bar']), {
-  foo: 'foo',
-  bar: undefined,
-})
-
-assert.deepEqual(getScriptTagVars('<html><script>var foo={ bar: 42, baz: { qux: "qux" } }</script></html>', ['foo', 'bar']), {
-  foo: { bar: 42, baz: { qux: 'qux' } },
-  bar: undefined,
-})
-
-assert.deepEqual(getScriptTagVars('<html><script>window.foo={ bar: 42, baz: { qux: "qux" } }</script></html>', ['window.foo', 'bar']), {
-  'window.foo': { bar: 42, baz: { qux: 'qux' } },
-  bar: undefined,
+test('Extracting vars and objects', (t) => {
+  t.plan(6)
+  t.deepEqual(getScriptTagVars('<html><script>var foo="foo"</script></html>', 'foo'), {
+    foo: 'foo',
+  })
+  t.deepEqual(getScriptTagVars('<html><script>var foo=null</script></html>', 'foo'), {
+    foo: null,
+  })
+  t.deepEqual(getScriptTagVars('<html><script></script></html>', 'foo'), {
+    foo: undefined,
+  })
+  t.deepEqual(getScriptTagVars('<html><script>var foo="foo"</script></html>', ['foo', 'bar']), {
+    foo: 'foo',
+    bar: undefined,
+  })
+  t.deepEqual(getScriptTagVars('<html><script>var foo={ bar: 42, baz: { qux: "qux" } }</script></html>', ['foo', 'bar']), {
+    foo: { bar: 42, baz: { qux: 'qux' } },
+    bar: undefined,
+  })
+  t.deepEqual(getScriptTagVars('<html><script>window.foo={ bar: 42, baz: { qux: "qux" } }</script></html>', ['window.foo', 'bar']), {
+    'window.foo': { bar: 42, baz: { qux: 'qux' } },
+    bar: undefined,
+  })
 })
